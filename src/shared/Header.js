d38cbfdeb588c257985627589b706ff3265bd5e1
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2021-11-20 21:27:51 +0900

    유튜브 클론코딩 1일차

---
+++ src/shared/Header.js
@@ -0,0 +1,7 @@
+import styles from './Header.module.css';
+
+function Header() {
+  return <div className={styles.he}>헤더!</div>;
+}
+
+export default Header;
---
+++ src/shared/Menu.js
@@ -0,0 +1,7 @@
+import styles from './Menu.module.css';
+
+function Menu() {
+  return <div className={styles.me}>메뉴!</div>;
+}
+
+export default Menu;
