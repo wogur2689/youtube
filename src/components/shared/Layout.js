f10eed3c369bddf893314cb7e2901e703751678a
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2021-11-25 23:28:49 +0900

    유튜브 클론코딩 3일차

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -1,14 +1,21 @@
 import styles from './Layout.module.css';
 import Header from './Header';
 import Menu from './Menu';
+import React, {useState} from 'react';
 
-function Layout({ children }) {
+function Layout({ children, activeMenu }) {
+  const [value, setValue] = useState(false);
+
+    function onClick() {
+      setValue(!value);
+    }
+
   return (
     <div className={styles.container}>
-      <Header />
+      <Header fold={onClick}/>
       <div className={styles.layout}>
-        <Menu />
-        <div className={styles.contents}>{children}</div>
+          {value === true ? <div><Menu activeMenu={activeMenu} /> <div className={styles.contents1}>{children}</div></div>
+          : <div><div className={styles.pad}/> <div className={styles.contents2}>{children}</div></div>}
       </div>
     </div>
   );
