4966bf13a1db16d69b85038cd6a0e7885868f89d
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2021-11-28 23:26:43 +0900

    유튜브 클론코딩 6일차

--- src/components/home/HomeCard.js
+++ src/components/home/HomeCard.js
@@ -1,4 +1,7 @@
+import moment from 'moment';
+import 'moment/locale/ko';
 import styles from './HomeCard.module.css';
+import { ProcessViewCount } from '../../utils';
 
 function HomeCard(data, index) {
   return (
@@ -25,8 +28,8 @@ function HomeCard(data, index) {
           <div className={styles.title}> {data.title}</div>
           <div className={styles.uploader}>{data.channelTitle}</div>
           <div className={styles.flex}>
-            <div className={styles.view}>{data.viewCount}</div>
-            <div className={styles.date}>{data.date}</div>
+          <div className={styles.view}>{ProcessViewCount(data.viewCount)}</div>
+            <div className={styles.date}>{moment(data.date).fromNow()}</div>
           </div>
         </div>
       </div>
