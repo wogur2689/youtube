25fdc32ee49f3e8464c031d12a958c6f3b090225
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2021-11-30 20:16:04 +0900

    유튜브 클론코딩 6일차

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 function ProcessViewCount(viewCount) {
     if (viewCount < 1000) {
       return `조회수 ${viewCount}회`; // 조회수 100회
@@ -10,6 +12,13 @@ function ProcessViewCount(viewCount) {
     } else {
       return `조회수 ${(viewCount / 100000000).toFixed(0)}억회`; // 조회수 3억회
     }
-  }
-  
-  export { ProcessViewCount };
+}
+
+function ProcessUploadDate() {
+  const date = moment('2021-09-15T13:15:02');
+  const standard = moment('2021-09-16T13:15:02');
+  const elapsed = date.from(standard);
+  return elapsed;
+}
+
+  export { ProcessViewCount, ProcessUploadDate };
